c9dbb6806da10f71057d6ed480f2762f67a81bcb
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-09 16:29:36 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const { GoogleAuth } = require('google-auth-library');
+
+async function main() {
+  const saKey = core.getInput('sa_key', { required: true });
+  const targetAudience = core.getInput('target_audience', { required: true });
+
+  const buffer = Buffer.from(saKey, 'base64');
+  const credentials = JSON.parse(buffer.toString('utf8'));
+  const client = await (new GoogleAuth({ credentials })).getIdTokenClient(targetAudience);
+  const { Authorization } = await client.getRequestHeaders();
+  core.setOutput('token', Authorization.replace(/Bearer /g, ''));
+}
+
+main().catch((e) => core.setFailed(e));
